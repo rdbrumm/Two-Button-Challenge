f82d24431ddaba9ac3e125f9ef56ecc6b90756e7
rdbrumm <rbrumm@zagmail.gonzaga.edu>
2020-09-08 09:35:15 -0700

    Initial Commit

---
+++ Two Button Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Two Button Challenge
+//
+//  Created by Rob Brumm on 9/8/20.
+//  Copyright © 2020 Rob Brumm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
