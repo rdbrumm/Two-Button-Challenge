348c46a143ac809942c4c0e939d5422da4144d04
rdbrumm <rbrumm@zagmail.gonzaga.edu>
2020-09-08 12:08:39 -0700

    completed 2.4 and 2.4.1

--- Two Button Challenge/ViewController.swift
+++ Two Button Challenge/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showAnotherMessageButtonPressed(_ sender: UIButton) {
+        messageLabel.text = "You are great!"
+        messageLabel.textColor = .systemRed
+        messageLabel.textAlignment = .right
+    }
+    
+    @IBAction func showMessageButtonPressed(_ sender: UIButton) {
+        messageLabel.text = "You are awesome!"
+        messageLabel.textColor = UIColor.blue
+        messageLabel.textAlignment = NSTextAlignment.left
 
+    }
 }
 
